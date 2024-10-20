a82b67208d93ac3b7207dc540fb1288fbf664198
Joseph <jcorona@synergydataanalytics.com>
2024-10-20 16:17:58 -0400

    Descripción del commit

--- hola_mundo.py
+++ hola_mundo.py
@@ -4,6 +4,11 @@ import os
 def main():
     nombre = os.getenv("USERNAME")
     print(f"¡Hola, {nombre} desde GitHub!")
+    # coloca joseph en una linea adicional
+    print("joseph")
+    # Coloca hola soy puta en una linea adicional
+    print("hola soy puta")
+    
 
 
 if __name__ == "__main__":
